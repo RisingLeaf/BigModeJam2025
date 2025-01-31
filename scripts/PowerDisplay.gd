97434786c3a108a377959c9ab1af95a768a74b28
thvk-net <control@thvk.net>
2025-01-31 12:05:49 +0100

    update v3

--- scripts/PowerDisplay.gd
+++ scripts/PowerDisplay.gd
@@ -3,6 +3,12 @@ extends Label
 @export
 var PlayerInst : Player
 
+
+var Viridis = [
+#	(0., Color.hex(0x440154),
+	#(0., Color.hex(0x440154)
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
